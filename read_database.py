19f4d5bccd7bb1f08eb8d15e7f7857d4403b679e
guanpei.li <1368647881@qq.com>
2014-05-22 14:31:38 +0800

    添加读取数据库的程序

---
+++ read_database.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import MySQLdb
+
+def main(title):
+    try:
+        conn=MySQLdb.connect(host='localhost',user='root',passwd='123456',port=3306,charset="utf8")
+        cur=conn.cursor()
+        conn.select_db('monitor')
+        cmd = 'select * from search_result_table where download_url = "%s";' % title
+        cur.execute(cmd)
+        result = cur.fetchall()
+        if len(result) == 0:
+            print '未找到影片信息'
+        else:
+            for i in result:
+                print i[1], i[2]
+                print i[3]
+                print 
+        conn.commit()
+        cur.close()
+        conn.close()
+    except Exception, e:
+        print e
+        
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        title = raw_input("请输入要查找的影片：")
+    else:
+        title = sys.argv[1]
+    main(title)
